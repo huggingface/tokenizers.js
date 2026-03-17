141988a43cbec4f9b51981508c88e7b551426e83
Joshua Lochner <26504141+xenova@users.noreply.github.com>
2026-03-16 21:06:06 -0400

    Add failing test

--- tests/models/deepseek_v3/test_tokenization_deepseek_v3.ts
+++ tests/models/deepseek_v3/test_tokenization_deepseek_v3.ts
@@ -9,4 +9,12 @@ export default {
       decoded: "<｜begin▁of▁sentence｜>How are you doing?",
     },
   },
+  "onnx-internal-testing/tiny-random-DeepseekV3ForCausalLM": {
+    SIMPLE: {
+      text: BASE_TEST_STRINGS.SIMPLE,
+      tokens: ["How", "Ġare", "Ġyou", "Ġdoing", "?"],
+      ids: [0, 4117, 477, 440, 4843, 33],
+      decoded: "<｜begin▁of▁sentence｜>How are you doing?",
+    },
+  },
 };
