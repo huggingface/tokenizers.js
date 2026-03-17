e6b738c7ebd5e9adf639c91e9d6cc16a08ee9193
Joshua Lochner <26504141+xenova@users.noreply.github.com>
2026-03-16 20:33:32 -0400

    Add unit test for deepseek_v3
    Ensure we produce the same correct behavior as transformers v4. https://github.com/huggingface/transformers/issues/44779

---
+++ tests/models/deepseek_v3/test_tokenization_deepseek_v3.ts
@@ -0,0 +1,12 @@
+import { BASE_TEST_STRINGS } from "../../static/constants";
+
+export default {
+  "hf-internal-testing/tiny-random-DeepseekV3ForCausalLM": {
+    SIMPLE: {
+      text: BASE_TEST_STRINGS.SIMPLE,
+      tokens: ['How', 'Ġare', 'Ġyou', 'Ġdoing', '?'],
+      ids: [0, 4117, 477, 440, 4843, 33],
+      decoded: "<｜begin▁of▁sentence｜>How are you doing?",
+    },
+  },
+};
